d85aa7fd42005af2a4c3656750e15cbd2a8fbdd8
KelvinM61 <kelvin2@Kelvins-MacBook-Pro-2.local>
2016-04-21 17:54:15 +0100

    tabs one and two
    structuring tabs one and two and bulk of code in both

--- js/app.js
+++ js/app.js
@@ -18,42 +18,52 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   $stateProvider
 
-    .state('tab', {
+    .state('tab', 
+  {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
 
 
-  .state('tab.one', {
+  .state('tab.one', 
+   {
     url: '/one',
-    views: {
-      'tab-one': {
+    views: 
+      {
+      'tab-one': 
+          {
         templateUrl: 'templates/tab-one.html',
         controller: 'oneCtrl'
+          }
       }
-    }
-  })
+    })
 
-  .state('tab.two', {
+  .state('tab.two', 
+     {
       url: '/two',
-      views: {
-        'tab-two': {
+      views: 
+      {
+        'tab-two': 
+          {
           templateUrl: 'templates/tab-two.html',
           controller: 'twoCtrl'
-        }
+          }
       }
     })
   
-  .state('tab.three', {
+  .state('tab.three', 
+    {
     url: '/three',
-    views: {
-      'tab-three': {
+    views: 
+      {
+      'tab-three': 
+          {
         templateUrl: 'templates/tab-three.html',
         controller: 'threeCtrl'
-      }
-    }
-  });
+          }
+     }
+   });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/one');
--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,26 @@
 angular.module('starter.controllers', [])
 
-.controller('oneCtrl', function($scope) {})
+.controller('oneCtrl', function($scope, ToDo) {
+  $scope.data = ToDo.data;
+})
 
-.controller('twoCtrl', function($scope) {})
+.controller('twoCtrl', function($scope, ToDo) {
+  var data = {
+    newToDo: ""
+      };
+
+  
+  function addToDo()  {
+    ToDo.addToDo(data.newToDo);
+    data.newToDo = "";
+    }
+  
+  $scope.data = data;
+  $scope.addToDo = addToDo;
+  })
 
 .controller('threeCtrl', function($scope){});
+;
+
+
+
